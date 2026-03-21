255035feca24077b7409d6225d0849de34c70d28
Michael Marsh <mike@marsh.pw>
2026-03-21 16:18:55 -0400

    argumentless command needs actions too

--- Micmarsh.CommandLine/Cmd.cs
+++ Micmarsh.CommandLine/Cmd.cs
@@ -4,12 +4,30 @@ using Micmarsh.CommandLine.Generator;
 namespace Micmarsh.CommandLine;
 
 
-public class Cmd(string Name, string Description)
+public class Cmd(string Name, string Description, IEnumerable<Command> SubCommands)
 {
-    public static Cmd New(string name, string desc) => new Cmd(name, desc);
+    public static Cmd New(string name, string desc) => new Cmd(name, desc,[]);
     
     public Cmd<A> AddOption<A>(Option<A> option) => new (Name, Description, new Opt<A>(option), []);
     public Cmd<A> AddArgument<A>(Argument<A> option) => new (Name, Description, new Arg<A>(option), []);
+    
+    public Command SetAction(Func<int> action)
+        => SetAction(_ => Task.FromResult(action()));
+    
+    public Command SetAction(Func<CancellationToken, Task<int>> action)
+    {
+        var result = ToCommand();
+        result.SetAction((_, ct) => action(ct));
+        return result;
+    }
+
+    public Command ToCommand()
+    {
+        var result = new Command(Name, Description);
+        foreach (var cmd in SubCommands) result.Subcommands.Add(cmd);
+        return result;
+    }
+
 }
 
 public static class CommandExt
