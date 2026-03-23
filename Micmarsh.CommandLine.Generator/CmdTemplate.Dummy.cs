8c7be91cf58baa7a0e56473e603d0020ee1e09a3
Michael Marsh <mike@marsh.pw>
2026-03-23 15:11:32 -0400

    comment on two-arg template

--- Micmarsh.CommandLine.Generator/CmdTemplate.Dummy.cs
+++ Micmarsh.CommandLine.Generator/CmdTemplate.Dummy.cs
@@ -2,6 +2,9 @@ using System.CommandLine;
 
 namespace Micmarsh.CommandLine.Generator;
 
+/// <summary>
+/// Exists only so <see cref="CmdTemplate{PLACEHOLDER}"/> can compile, and thus remain more useful than a plantext file
+/// </summary>
 public record CmdTemplate<A, B>(
     string name, 
     string desc,
