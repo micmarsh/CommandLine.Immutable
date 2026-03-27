bcd27e2f4d4aef245fac9ebdd91ecd7969f6f53d
Michael Marsh <mike@marsh.pw>
2026-03-27 12:04:41 -0400

    long overdue shell script

---
+++ generate.sh
@@ -0,0 +1,2 @@
+
+./CommandLine.Immutable.Generator/bin/Debug/net10.0/CommandLine.Immutable.Generator -t CommandLine.Immutable.Generator/CmdTemplate.cs -n 12 > CommandLine.Immutable/Cmd.Generated.cs
