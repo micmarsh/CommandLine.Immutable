64e97dd4ceacc2467d45ead44d4f2d76065326e5
Michael Marsh <mike@marsh.pw>
2026-03-27 16:14:57 -0400

    cleanup example input

--- CommandLine.Immutable.LearnExample/Inputs.cs
+++ CommandLine.Immutable.LearnExample/Inputs.cs
@@ -1,60 +1,49 @@
-using System.CommandLine;
+using System.CommandLine.Parsing;
+using LanguageExt;
+using CommandLine.Immutable;
+using LanguageExt.Common;
 
 namespace CommandLine.Immutable.LearnExample;
 
 public static class Inputs
 {
-    public static Option<FileInfo> fileOption = new("--file")
-    {
-        Description = "An option whose argument is parsed as a FileInfo",
-        Required = true,
-        DefaultValueFactory = result =>
-        {
-            if (result.Tokens.Count == 0)
-            {
-                return new FileInfo("sampleQuotes.txt");
+    public static System.CommandLine.Option<FileInfo> fileOption = new System.CommandLine.Option<FileInfo>("--file")
+        .With(Description: "An option whose argument is parsed as a FileInfo",
+            Required: true,
+            Recursive: true,
+            DefaultValueFactory: result =>
+                result.Tokens.Match(() => new FileInfo("sampleQuotes.txt"),
+                    filePath => !File.Exists(filePath.Value)
+                        ? (Fin<FileInfo>) Error.New("File does not exist")
+                        : new FileInfo(filePath.Value),
+                    (_1, _2) => throw new Exception("fileOption somehow parsed more than one token")));
 
-            }
-            string filePath = result.Tokens.Single().Value;
-            if (!File.Exists(filePath))
-            {
-                result.AddError("File does not exist");
-                return null;
-            }
-            else
-            {
-                return new FileInfo(filePath);
-            }
-        },
-        Recursive = true
-    };
-
-    public static Option<int> delayOption = new("--delay")
+    public static System.CommandLine.Option<int> delayOption = new("--delay")
     {
         Description = "Delay between lines, specified as milliseconds per character in a line",
         DefaultValueFactory = parseResult => 42
     };
-    public static  Option<ConsoleColor> fgcolorOption = new("--fgcolor")
+    public static  System.CommandLine.Option<ConsoleColor> fgcolorOption = new("--fgcolor")
     {
         Description = "Foreground color of text displayed on the console",
         DefaultValueFactory = parseResult => ConsoleColor.White
     };
-    public static Option<bool> lightModeOption = new("--light-mode")
+    public static System.CommandLine.Option<bool> lightModeOption = new("--light-mode")
     {
         Description = "Background color of text displayed on the console: default is black, light mode is white"
     };
 
-    public static Option<string[]> searchTermsOption = new("--search-terms")
+    public static System.CommandLine.Option<string[]> searchTermsOption = new("--search-terms")
     {
         Description = "Strings to search for when deleting entries",
         Required = true,
         AllowMultipleArgumentsPerToken = true
     };
-    public static Argument<string> quoteArgument = new("quote")
+    public static System.CommandLine.Argument<string> quoteArgument = new("quote")
     {
         Description = "Text of quote."
     };
-    public static Argument<string> bylineArgument = new("byline")
+    public static System.CommandLine.Argument<string> bylineArgument = new("byline")
     {
         Description = "Byline of quote."
     };
