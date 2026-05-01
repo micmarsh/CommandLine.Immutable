ed6c1f9c35ba176209fc143210959e482d9ae9e9
Michael Marsh <mike@marsh.pw>
2026-05-01 17:08:16 -0400

    failing tests, optional inputs where always broken?

---
+++ CommandLine.Immutable.Tests/OptionalInputTests.cs
@@ -0,0 +1,27 @@
+﻿using LanguageExt;
+
+namespace CommandLine.Immutable.Tests;
+
+public class OptionalInputTests
+{
+    private const int DefaultProgramReturn = 0;
+    private static readonly Cmd<Option<int>> OptionalOptTestProgram = Cmd.New("test", "test")
+        .AddOption(OptionalInput.Opt<int>("--test-int", "-ti"))
+        .WithAction(test => test.IfNone(DefaultProgramReturn));
+
+    [Fact]
+    public void OptionalOption_WhenProvided_ShouldNotInfiniteLoop()
+    {
+        var testProgram = OptionalOptTestProgram;
+        
+        Assert.Equal(3, testProgram.ToRoot().Parse(["test", "-ti", "3"]).Invoke());
+    }
+    
+    [Fact]
+    public void OptionalOption_WhenNotProvided_ShouldReturnDefault()
+    {
+        var testProgram = OptionalOptTestProgram;
+        
+        Assert.Equal(DefaultProgramReturn, testProgram.ToRoot().Parse(["test"]).Invoke());
+    }
+}
