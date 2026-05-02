05ad18b59c5591c7b6859db78d6010786bd584cd
Michael Marsh <mike@marsh.pw>
2026-05-01 21:32:16 -0400

    WIP passes tests but excludes FileInfo, need to basically re-implement internal StringConverters?

--- CommandLine.Immutable.LangExt/Inputs/OptionalInput.cs
+++ CommandLine.Immutable.LangExt/Inputs/OptionalInput.cs
@@ -5,17 +5,28 @@ namespace CommandLine.Immutable;
 
 public static class OptionalInput
 {
-    public static System.CommandLine.Option<Option<T>> Opt<T>(string name, params string[] aliases) =>
+    public static System.CommandLine.Option<Option<T>> Opt<T>(string name, params string[] aliases) 
+        where T : IParsable<T> =>
         new (name, aliases)
         {
             DefaultValueFactory = _ => None,
+            CustomParser = r => r.Tokens switch
+            {
+                [] => None,
+                [var token, ..] => Some(T.Parse(token.Value, null))
+            },
             Required = false
         };
     
-    public static System.CommandLine.Argument<Option<T>> Arg<T>(string name) =>
+    public static System.CommandLine.Argument<Option<T>> Arg<T>(string name) 
+        where T : IParsable<T> =>
         new (name)
         {
             DefaultValueFactory = _ => None,
-            CustomParser = r => Optional(r.GetValueOrDefault<T>())
+            CustomParser = r => r.Tokens switch
+            {
+                [] => None,
+                [var token, ..] => Some(T.Parse(token.Value, null))
+            },
         };   
 }
